249df0d23aed2cc7ae913856d5831cff604b299f
instagramcuanta82-wq <instagramcuanta82@gmail.com>
2025-12-06 18:07:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ wss.on("connection", (ws) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 server.listen(PORT, () => {
   console.log("Servidor WebSocket activo en puerto", PORT);
 });
